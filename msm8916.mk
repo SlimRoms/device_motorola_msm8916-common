99a13e6105df0e680cb5b1e3d414aef226a65aad
Alberto97 <albertop2197@gmail.com>
2017-10-06 14:33:38 +0200

    msm8916-common: CMActions -> LineageActions
    Since history is going to be killed anyway:
     * Move related stuff under "actions" or "doze" packages, make every class role clear
     * Rename misleading TouchscreenGesture* to Actions*
    
    Also add original-package for preferences migration
    
    Change-Id: I721a39a9d3c3acd7f778b491678350e4bbf0be17

--- msm8916.mk
+++ msm8916.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8916
 
+# LineageActions
+PRODUCT_PACKAGES += \
+    LineageActions
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
