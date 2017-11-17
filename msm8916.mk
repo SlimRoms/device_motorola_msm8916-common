a18ef7e0b41f05c0abbd40e7d9f50db7705b2109
Alberto97 <albertop2197@gmail.com>
2017-11-17 20:14:14 +0100

    msm8916-common: Import missing DRM HIDL hal
    Change-Id: I5bf0423803e9655889d4f26f1c83423b52ff9d86

--- msm8916.mk
+++ msm8916.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CMActions
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Ebtables
 PRODUCT_PACKAGES += \
     ebtables \
